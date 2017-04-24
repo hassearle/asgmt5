70ba30d5218509e3799a6640e0291f8025959d4b
Ash <a.searle274@gmail.com>
2017-04-24 17:55:40 -0500

    completed skeleton code (minus random number generator)

--- main.adb
+++ main.adb
@@ -24,7 +24,143 @@ procedure Main is
 	BTI : INTEGER;
 	ITB : BINARY_ARRAY;
 
+
+   --=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+   -- DEFINITION 
+   --=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+
+		-- convert binary value to int equivalent 
+		-- must implemented with a loop
+		function Bin_To_Int(X : BINARY_ARRAY) return INTEGER; 
+
+		-- convert int to binary value equivalent 
+		-- limits size of int to 16 bits 
+		-- binary array should order the least significant bit to the far right
+		function Int_To_Bin(X : INTEGER) return BINARY_ARRAY;
+
+		-- returns sum of two binary arrays 
+		-- dont worry about overflows
+		function Plus_Overload(First, Second : BINARY_ARRAY)
+			return BINARY_ARRAY;
+
+		-- returns sum of binary array and int
+		function Plus_Overload(X : BINARY_ARRAY; Y : INTEGER) 
+			return BINARY_ARRAY;
+
+		-- returns the difference of two binary arrays
+		-- subtract the second from the first parameter
+		function Minus_Overload(X,Y : BINARY_ARRAY) 
+			return BINARY_ARRAY;
+
+
+		-- returns the difference of binary array and int
+		-- subtract the second from the first parameter
+		function Minus_Overload(X : BINARY_ARRAY; Y : INTEGER) 
+			return BINARY_ARRAY;
+
+
+		-- reverse array
+		procedure Reverse_Bin_Arr(X : BINARY_ARRAY);
+
+
+		-- prints binary array to console
+		procedure Print_Bin_Arr(X : BINARY_ARRAY);  
+		
+	
+   --=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+   --IMPLEMENTATION 
+   --=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+
+		function 
+
+
+		function Bin_To_Int(X : BINARY_ARRAY) return INTEGER is
+			Temp : INTEGER;
+			begin
+				Temp := 1;
+				return Temp; 
+			end Bin_To_Int;	
+
+
+		function Int_To_Bin(X : INTEGER) return BINARY_ARRAY is
+			Temp : BINARY_ARRAY;
+			begin
+				--Temp := BINARY_ARRAY(1);
+				return Temp;
+			end Int_To_Bin;	
+			
+		function Plus_Overload(First, Second : BINARY_ARRAY) return BINARY_ARRAY is
+			Temp : BINARY_ARRAY;
+			begin
+				--Temp := BINARY_ARRAY(1);
+				return Temp;
+			end Plus_Overload;
+
+		function Plus_Overload(X : BINARY_ARRAY; Y : INTEGER) return BINARY_ARRAY is
+			Temp : BINARY_ARRAY;
+			begin
+				--Temp := BINARY_ARRAY(1);
+				return Temp;
+			end Plus_Overload;		
+
+
+		function Minus_Overload(X,Y : BINARY_ARRAY) return BINARY_ARRAY is
+			Temp : BINARY_ARRAY;
+			begin
+				--Temp := BINARY_ARRAY(1);
+				return Temp;
+			end Minus_Overload;	
+
+
+		function Minus_Overload(X : BINARY_ARRAY; Y : INTEGER) return BINARY_ARRAY is
+			Temp : BINARY_ARRAY;
+			begin
+				--Temp := BINARY_ARRAY(1);
+				return Temp;
+			end Minus_Overload;
+
+
+		procedure Reverse_Bin_Arr(X : BINARY_ARRAY) is
+			Temp : INTEGER;
+			begin
+				Temp := 1;
+
+			end Reverse_Bin_Arr;
+
+		procedure Print_Bin_Arr(X : BINARY_ARRAY) is
+			Temp : INTEGER;
+			begin
+			 	Temp := 1;
+
+			end Print_Bin_Arr; 
+
+ -- --=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+	
+	
+
 begin
+	
+   --=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+   --START OF MAIN
+   --=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+
+		Put("hello");
+
+		-- BTI := Bin_To_Int(A_Array);
+		-- Put(BTI);
+		-- B_Array := Int_To_Bin;
+		-- Print_Bin_Arr(B_Array); 
+		-- C_Array := Plus_Overload(A_Array : B_Array);
+		-- Put_Line(C_Array);
+		-- D_Array := Minus_Overload(A_Array : B_Array);
+		-- Put_Line(D_Array);
+		-- Reverse_Bin_Arr(D_Array);
+		-- Put_Line(D_Array);
+
+end Main;
+
+
+
 
 	-- for I in BINARY_ARRAY'Range loop --SOURCE [1]
 	-- 	A_Array(I) := Random_Number;
