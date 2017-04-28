908eb618dad0355232567ea6a10d4ce9cb3a9999
Ash <a.searle274@gmail.com>
2017-04-28 17:14:12 -0500

    converted project into three files

--- asgmt5.adb
+++ asgmt5.adb
@@ -1,80 +1,180 @@
-with Ada.Text_IO, Ada.Integer_Text_IO, 
-		Ada.Numerics.discrete_Random;
-use Ada.Text_IO, Ada.Integer_Text_IO;
+	with Ada.Text_IO, Ada.Integer_Text_IO,Ada.Numerics.discrete_Random,
+		Ada.Strings.Unbounded;
+	use Ada.Text_IO, Ada.Integer_Text_IO, Ada.Strings.Unbounded;
 
 package body Asgmt5 is
 
+	--=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+    --IMPLEMENTATION 
+    --=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
 
-	function Bin_To_Int(X : BINARY_ARRAY) return INTEGER is
-		Temp : INTEGER;
-		begin
-			Temp := 1;
-			return Temp; 
-		end Bin_To_Int;	
+		function Bin_To_Int(X : BINARY_ARRAY) return INTEGER is
+			Int : INTEGER;
+			Intt: INTEGER;
+				begin
+				for I in BINARY_ARRAY'Range loop
+					if X(I) = 1 then
+						Intt := 15 - I;
+						Int := Int + 2**(Intt);
+					end if;
+				end loop;
+				return Int; 
+			end Bin_To_Int;	
 
+		function Int_To_Bin(X : INTEGER) return BINARY_ARRAY is --SOURCE[4]
+			Temp : BINARY_ARRAY;
+			Divided : INTEGER;
+			Remainder : INTEGER;
+			begin
+				Divided := X;
+				for I in BINARY_ARRAY'Range loop
+					if Divided rem 2 = 0 then
+						Remainder := 0;
+						else Remainder := 1;
+					end if;
+					Divided := Divided / 2;
+					Temp(15 - I) := BINARY_NUMBER(Remainder);
+				end loop;
+				return Temp;
+			end Int_To_Bin;	
+			
+		function Plus_Overload(First, Second : BINARY_ARRAY) return BINARY_ARRAY is
+			Temp : BINARY_ARRAY;
+			Remain : BINARY_NUMBER := 0;
+			begin
+				for I in BINARY_ARRAY'Range loop
+					if First(15 - I) = 1 and Second(15 - I) = 1 then
+					 	case Remain is
+					 		when 0 =>
+					 			Temp(15 - I) := 0;
+					 			Remain := 1; 
+					 		when 1 =>
+					 			Temp(15 - I) := 1;
+					 			Remain := 1;		
+					 	end case;		 		
+					 end if;
 
-	function Int_To_Bin(X : INTEGER) return BINARY_ARRAY is
-		Temp : BINARY_ARRAY;
-		begin
-			--Temp := BINARY_ARRAY(1);
-			return Temp;
-		end Int_To_Bin;	
+					if (First(15 - I) = 0 and Second(15 - I) = 1) or 
+						(First(15 - I) = 1 and Second(15 - I) = 0)
+						 then
+					 	case Remain is
+					 		when 0 =>
+					 			Temp(15 - I) := 1;
+					 			Remain := 0; 
+					 		when 1 =>
+					 			Temp(15 - I) := 0;
+					 			Remain := 1;		
+					 	end case;
+					 end if; 
 
+					if First(15 - I) = 0 and Second(15 - I) = 0 then
+					 	case Remain is
+					 		when 0 =>
+					 			Temp(15 - I) := 0;
+					 			Remain := 0; 
+					 		when 1 =>
+					 			Temp(15 - I) := 1;
+					 			Remain := 0;		
+					 	end case;
+					 		
+					 end if;
+					
+				end loop;
+				return Temp;
+			end Plus_Overload;
 
-	function Plus_Overload(First, Second : BINARY_ARRAY) return BINARY_ARRAY is
-		Temp : BINARY_ARRAY;
-		begin
-			--Temp := BINARY_ARRAY(1);
-			return Temp;
-		end Plus_Overload;
+		function Plus_Overload(X : BINARY_ARRAY; Y : INTEGER) return BINARY_ARRAY is
+			Temp : BINARY_ARRAY;
+			begin
+				Temp := Int_To_Bin(Y);
+				return Plus_Overload(X, Temp);
+			end Plus_Overload;		
 
+		-- SOURCE [6]
+		function Minus_Overload(X,Y : BINARY_ARRAY) return BINARY_ARRAY is
+			Temp : BINARY_ARRAY;
+			Temp_Plus_One : BINARY_ARRAY;
+			Add_One_Array : BINARY_ARRAY := (0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1);
+			Final_Diff : BINARY_ARRAY;
+			begin
+				for I in BINARY_ARRAY'Range loop
+					if Y(I) = 1 then Temp(I) := 0; end if;
+					if Y(I) = 0 then Temp(I) := 1; end if;					
+				end loop;
+				Temp_Plus_One := Plus_Overload(Temp, Add_One_Array);
+				Final_Diff := Plus_Overload(X, Temp_Plus_One);
+				return Final_Diff;
+			end Minus_Overload;	
 
-	function Plus_Overload(X : BINARY_ARRAY; Y : INTEGER) return BINARY_ARRAY is
-		Temp : BINARY_ARRAY;
-		begin
-			--Temp := BINARY_ARRAY(1);
-			return Temp;
-		end Plus_Overload;		
+		function Minus_Overload(X : BINARY_ARRAY; Y : INTEGER) return BINARY_ARRAY is
+			Temp : BINARY_ARRAY;
+			begin
+				Temp := Int_To_Bin(Y);
+				return Minus_Overload(Temp, X);
+			end Minus_Overload;
 
+		procedure Reverse_Bin_Arr(X : BINARY_ARRAY) is
+			Temp : BINARY_ARRAY;
+			begin
+				for I in BINARY_ARRAY'Range  loop
+					if X(I) = 1 then Temp(I) := 0; end if;
+					if X(I) = 0 then Temp(I) := 1; end if;			
+				end loop;
 
-	function Minus_Overload(X,Y : BINARY_ARRAY) return BINARY_ARRAY is
-		Temp : BINARY_ARRAY;
-		begin
-			--Temp := BINARY_ARRAY(1);
-			return Temp;
-		end Minus_Overload;	
+				F_Array := Temp;
+			end Reverse_Bin_Arr;
 
+		procedure Print_Bin_Arr(X : BINARY_ARRAY) is
+			begin
+			for I in BINARY_ARRAY'Range  loop
+					Put(INTEGER'Image(INTEGER(X(I))));
+				end loop;
+			end Print_Bin_Arr; 
 
-	function Minus_Overload(X : BINARY_ARRAY; Y : INTEGER) return BINARY_ARRAY is
-		Temp : BINARY_ARRAY;
-		begin
-			--Temp := BINARY_ARRAY(1);
-			return Temp;
-		end Minus_Overload;
+        --=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+        -- HELPER FUNCTIONS
+        --=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+		
+		-- random binary number generator
+		function Random_Binary return BINARY_NUMBER is--SOURCE [2]
+			package Rand_Int is new Ada.Numerics.discrete_Random(BINARY_NUMBER);
+			use Rand_Int;
+			G : Generator;
+			NUM : BINARY_NUMBER; 
+			begin
+				Reset(G);
+				NUM := Random(G);
+				return NUM;
+			end Random_Binary;
 
+		-- random integer number generator 
+		function Random_Number return INTEGER is
+			type NUM is range 0..32767; --half so that sums can be binary
+			NUMM : NUM; 
+			INT : INTEGER;
+			package Rand_Int is new Ada.Numerics.discrete_Random(NUM);
+			use Rand_Int;
+			G : Generator;
+			begin -- Random_Number
+				Reset(G);
+				NUMM := Random(G);
+				INT := INTEGER(NUMM);
+				return INT;
+			end Random_Number;
 
-	procedure Reverse_Bin_Arr(X : BINARY_ARRAY) is
-		Temp : INTEGER;
-		begin
-			Temp := 1;
-
-		end Reverse_Bin_Arr;
-
-	procedure Print_Bin_Arr(X : BINARY_ARRAY) is
-		Temp : INTEGER;
-		begin
-		 	Temp := 1;
-
-		end Print_Bin_Arr; 
-
---=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
-
-	function Random_Number return BINARY_NUMBER is
-		Temp : BINARY_NUMBER;
-		begin
-			Temp := BINARY_NUMBER(1);
-			return Temp;
-		end Random_Number;
-
+		-- high random integer number generator 
+		function Top_Random_Number return INTEGER is
+			type NUM is range 32767..65535; 
+			NUMM : NUM; 
+			INT : INTEGER;
+			package Rand_Int is new Ada.Numerics.discrete_Random(NUM);
+			use Rand_Int;
+			G : Generator;
+			begin -- Random_Number
+				Reset(G);
+				NUMM := Random(G);
+				INT := INTEGER(NUMM);
+				return INT;
+			end Top_Random_Number;
 
 end Asgmt5;
--- asgmt5.ads
+++ asgmt5.ads
@@ -1,55 +1,76 @@
--- [1] Provided Samples: random.ads, Line 40
+	package Asgmt5 is
 
-generic 
-	type BINARY_NUMBER is range <>;
-	type BINARY_ARRAY is array(INTEGER) of BINARY_NUMBER;
+		type BINARY_NUMBER is range 0..1;
+		type BINARY_ARRAY is array(INTEGER range 0..15) of BINARY_NUMBER;
 
-package Asgmt5 is 
+		A_B_Sum : INTEGER; --A_Array + B_Array
+		A_B_Dif : INTEGER; --A_Array - B_Array
+		A_R_Sum : INTEGER; --A_Array + Random Integer 
+		R_A_Dif : INTEGER; --Random Integer - A_Array
+		C_R_Sum : INTEGER; --C_Array + Random Integer 
+		A_Random_Int: Integer;
+		B_Random_Int: Integer;
+		C_Random_Int: Integer;	
+		Zero_Array: BINARY_ARRAY := (0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0);
+		-- A_Int : INTEGER;
+		A_Array: BINARY_ARRAY;
+		Temp : BINARY_NUMBER;
+		B_Array: BINARY_ARRAY;
+		C_Array: BINARY_ARRAY;
+		D_Array: BINARY_ARRAY;
+		E_Array: BINARY_ARRAY;
+		F_Array: BINARY_ARRAY;
+		BTI : INTEGER;
 
-	-- convert binary value to int equivalent 
-	-- must implemented with a loop
-	function Bin_To_Int(X : BINARY_ARRAY) return INTEGER; 
+	--=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+    -- DEFINITION 
+    --=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+
+		-- convert binary value to int equivalent 
+		-- must implemented with a loop
+		function Bin_To_Int(X : BINARY_ARRAY) return INTEGER; 
+
+		-- convert int to binary value equivalent 
+		-- limits size of int to 16 bits 
+		-- binary array should order the least significant bit to the far right
+		function Int_To_Bin(X : INTEGER) return BINARY_ARRAY;
+
+		-- returns sum of two binary arrays 
+		-- dont worry about overflows
+		function Plus_Overload(First, Second : BINARY_ARRAY)
+			return BINARY_ARRAY;
+
+		--returns sum of binary array and int
+		function Plus_Overload(X : BINARY_ARRAY; Y : INTEGER) 
+			return BINARY_ARRAY;
+
+		-- returns the difference of two binary arrays
+		-- subtract the second from the first parameter
+		function Minus_Overload(X,Y : BINARY_ARRAY) 
+			return BINARY_ARRAY;
 
 
-	-- convert int to binary value equivalent 
-	-- limits size of int to 16 bits 
-	-- binary array should order the least significant bit to the far right
-	function Int_To_Bin(X : INTEGER) return BINARY_ARRAY;
-
-	
-	-- returns sum of two binary arrays 
-	-- dont worry about overflows
-	function Plus_Overload(First, Second : BINARY_ARRAY)
-		return BINARY_ARRAY;
-
-	-- returns sum of binary array and int
-	function Plus_Overload(X : BINARY_ARRAY; Y : INTEGER) 
-		return BINARY_ARRAY;
-
-	-- returns the difference of two binary arrays
-	-- subtract the second from the first parameter
-	function Minus_Overload(X,Y : BINARY_ARRAY) 
-		return BINARY_ARRAY;
+		-- returns the difference of binary array and int
+		-- subtract the second from the first parameter
+		function Minus_Overload(X : BINARY_ARRAY; Y : INTEGER) 
+			return BINARY_ARRAY;
 
 
-	-- returns the difference of binary array and int
-	-- subtract the second from the first parameter
-	function Minus_Overload(X : BINARY_ARRAY; Y : INTEGER) 
-		return BINARY_ARRAY;
+		-- reverse array
+		procedure Reverse_Bin_Arr(X : BINARY_ARRAY);
 
 
-	-- reverse array
-	procedure Reverse_Bin_Arr(X : BINARY_ARRAY);
+		-- prints binary array to console
+		procedure Print_Bin_Arr(X : BINARY_ARRAY);  
+		
+     --=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+     --HELPER FUNCTION DEFINITIONS
+     --=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
 
+		function Random_Binary return BINARY_NUMBER;
 
-	-- prints binary array to console
-	procedure Print_Bin_Arr(X : BINARY_ARRAY); 
+		function Random_Number return INTEGER;
 
-
---=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
-	
-	-- random number generator
-	function Random_Number return BINARY_NUMBER; --SOURCE [1]
-
+		function Top_Random_Number return INTEGER;
 
 end Asgmt5;
